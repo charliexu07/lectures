d00c5487acf8e8db54eb5bb7de26a33b66f3dd05
Jiachen Xu <xujiachen1999@outlook.com>
2020-02-19 22:13:53 -0600

    Update producer_consumer.c

--- lecture-13/code/producer_consumer.c
+++ lecture-13/code/producer_consumer.c
@@ -6,40 +6,58 @@
 
 #define BUFFER_SIZE 10
 
-int producer_index = 0;
-int consumer_index = 0;
+//int producer_index = 0;
+//int consumer_index = 0;
 int num_items = 0;
-int buffer[BUFFER_SIZE];
+//int buffer[BUFFER_SIZE];
 
-int counter = 0;
+//int counter = 0;
+
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_empty = PTHREAD_COND_INITIALIZER;
 
 // function for producer threads
 void *producer_function(void *arg)
 {
-    // make this function thread safe and efficient using condition variables!
-    while (1) {
-        buffer[producer_index++] = counter++;
-        producer_index %= BUFFER_SIZE;
-        num_items++;
+    pthread_mutex_lock(&mtx);
+    while (num_items >= BUFFER_SIZE) {
+        pthread_cond_wait(&cond_empty, &mtx);
     }
+    num_items++;
+    pthread_mutex_unlock(&mtx);
+    pthread_cond_signal(&cond_full);
+//     // make this function thread safe and efficient using condition variables!
+//     while (1) {
+//         buffer[producer_index++] = counter++;
+//         producer_index %= BUFFER_SIZE;
+//         num_items++;
+//     }
   
-    return NULL;
+//     return NULL;
 }
 
 // function for producer threads
 void *consumer_function(void *arg)
 {
-    char *name = (char *)arg;
-
-    // make this function thread safe and efficient using condition variables!
-    while (1) {
-        int x = buffer[consumer_index++];
-        consumer_index %= BUFFER_SIZE;
-        num_items--;
-        printf("%s consumed %d\n", name, x);
+    pthread_mutex_lock(&mtx);
+    while (num_items <= 0) {
+        pthread_cond_wait(&cond_full, &mtx);
     }
+    num_items--;
+    pthread_mutex_unlock(&mtx);
+    pthread_cond_signal(&cond_empty);
+//     char *name = (char *)arg;
 
-      return NULL;
+//     // make this function thread safe and efficient using condition variables!
+//     while (1) {
+//         int x = buffer[consumer_index++];
+//         consumer_index %= BUFFER_SIZE;
+//         num_items--;
+//         printf("%s consumed %d\n", name, x);
+//     }
+
+//       return NULL;
 }
                                                                              
 int main(int argc, char *argv[])
